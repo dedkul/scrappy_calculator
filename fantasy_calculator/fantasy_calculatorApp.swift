5ecf3a1d464f8ea485a9e3f1f327543c82911744
Ashish <ashujangra005@gmail.com>
2026-03-16 03:12:33 +1100

    Initial Commit

---
+++ fantasy_calculator/fantasy_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  fantasy_calculatorApp.swift
+//  fantasy_calculator
+//
+//  Created by Ashish Jangra on 16/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct fantasy_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
